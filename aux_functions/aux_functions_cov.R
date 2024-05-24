3e85f0365eed72b64806cc69236cb5ba87e00958
David Bolin <bolinjd@KL-26412.local>
2024-05-24 17:57:38 +0300

    update nn prediction and estimation

--- aux_functions/aux_functions_cov.R
+++ aux_functions/aux_functions_cov.R
@@ -2,6 +2,74 @@
 
 library(rSPDE)
 
+nn.like <- function(theta, loc, n.nbr, Y,nu = NULL) {
+    kappa = exp(theta[1])
+    sigma = exp(theta[2])
+    if(length(theta)==4) { 
+        nu = exp(theta[3])
+        sigma.e = exp(theta[4])
+    } else {
+        sigma.e = exp(theta[3])    
+    }
+
+    tmp <- get.nn(loc,kappa,nu,sigma,n.nbr)
+    
+    n <- length(Y)
+    
+    prior.ld <- 0.5 * sum(log(diag(tmp$Fi)))
+    
+    Q.post <- t(tmp$Bs)%*%tmp$Fi%*%tmp$Bs + Diagonal(n) / sigma.e^2
+    
+    
+    R.post <- Matrix::Cholesky(Q.post)
+    
+    posterior.ld <-  c(determinant(R.post, logarithm = TRUE, sqrt = TRUE)$modulus)
+    
+    AtY <- Y / sigma.e^2
+    
+    mu.post <- solve(R.post, AtY, system = "A")
+    
+    lik <- prior.ld - posterior.ld - n * log(sigma.e)
+    v <- tmp$Bs %*% mu.post
+    lik <- lik - 0.5 * (t(v) %*% tmp$Fi %*% v +
+                            t(Y - mu.post) %*% (Y - mu.post) / sigma.e^2)
+    
+    return(-as.double(lik))
+}
+
+rat.like <- function(theta, loc, m, Y,nu = NULL) {
+    
+    kappa = exp(theta[1])
+    sigma = exp(theta[2])
+    if(length(theta)==4) { 
+        nu = exp(theta[3])
+        sigma.e = exp(theta[4])
+    } else {
+        sigma.e = exp(theta[3])    
+    }
+    tmp <- matern.rational.ldl(loc = loc, order = m, nu = nu, kappa = kappa, sigma = sigma, 
+                               type_rational = "brasil", type_interp =  "spline")    
+    n <- length(Y)
+    
+    prior.ld <- 0.5 * sum(log(diag(tmp$D)))
+    
+    Q.post <- t(tmp$L)%*%tmp$D%*%tmp$L + t(tmp$A) %*% tmp$A / sigma.e^2
+    
+    R.post <- Matrix::Cholesky(Q.post)
+    
+    posterior.ld <- c(determinant(R.post, logarithm = TRUE, sqrt = TRUE)$modulus)
+    
+    AtY <- t(tmp$A) %*% Y / sigma.e^2
+    
+    mu.post <- solve(R.post, AtY, system = "A")
+    
+    lik <- prior.ld - posterior.ld - n * log(sigma.e)
+    v <- tmp$L %*% mu.post
+    lik <- lik -  0.5 * (t(v) %*% tmp$D %*% v +
+                             t(Y - tmp$A %*% mu.post) %*% (Y - tmp$A %*% mu.post) / sigma.e^2)
+    
+    return(-as.double(lik))
+}
 # Sparse matrices
 
 library(Matrix)
@@ -44,20 +112,33 @@ get_m <- function(nu, m, method = c("nngp", "statespace", "kl"), type = c("predi
 
 ## NN GP - nearest neighbor
 
-get.neighbor <- function(i, n.nbr) {
-    if(i-n.nbr <= 0) {
-        if(i==1) {
-            return(numeric())
+get.neighbor <- function(i, n.nbr, S = NULL) {
+    if(is.null(S)) {
+        S = 1:i
+        S.i <- sort(S[S<i], decreasing = TRUE)
+        if(length(S.i) < n.nbr) {
+            return(sort(S.i))
         } else {
-            return(1:(i-1))    
-        }
-        
+            return(sort(S.i[1:n.nbr]))
+        }    
     } else {
-        return((i-n.nbr):(i-1))
+        if(i %in% S) { 
+            S.i <- sort(S[S<i], decreasing = TRUE)
+            if(length(S.i) < n.nbr) {
+                return(sort(S.i))
+            } else {
+                return(sort(S.i[1:n.nbr]))
+            }    
+        } else {
+             dists <- abs(i - S)
+             return(sort(sort(dists, index.return = TRUE)$ix[1:n.nbr]))
+        }
     }
+    
+    
 }
 
-get.nn <- function(loc,kappa,nu,sigma, n.nbr) {
+get.nn <- function(loc,kappa,nu,sigma, n.nbr, S = NULL) {
     k <- length(loc)
     ii <- numeric((2*k-n.nbr)*(n.nbr+1)/2)
     jj <- numeric((2*k-n.nbr)*(n.nbr+1)/2)
@@ -72,23 +153,36 @@ get.nn <- function(loc,kappa,nu,sigma, n.nbr) {
     jj[1] <- 1
     counter <- 1
     for(i in 2:k) {
-        nbrs <- get.neighbor(i,n.nbr)
-        Sigma.in <- matern.covariance(h = abs(loc[nbrs]-loc[i]),
-                                      kappa = kappa, nu = nu, sigma = sigma)
-        Sigma.nn <- matern.covariance(h = as.matrix(dist(loc[nbrs])),
-                                      kappa = kappa, nu = nu, sigma = sigma)
-        N <- length(nbrs) + 1
-        tmp <- solve(Sigma.nn, Sigma.in)
-        val[counter + (1:N)] <- c(-t(tmp),1)
-        ii[counter + (1:N)] <- rep(i,N)
-        jj[counter + (1:N)] <- (i-N+1):i
-        Fs.d[i] <- sigma^2 - t(Sigma.in)%*%tmp
-        Fsi.d[i] <- 1/Fs.d[i]
+        nbrs <- get.neighbor(i,n.nbr, S)
+        if(isempty(nbrs)) {
+            Fs.d[i] <- sigma^2
+            Fsi.d[i] <- 1/sigma^2
+            val[counter +1] <- 1
+            ii[counter+1] <- i
+            jj[counter+1] <- i
+            counter = counter + 1
+        } else {
+            Sigma.in <- matern.covariance(h = abs(loc[nbrs]-loc[i]),
+                                          kappa = kappa, nu = nu, sigma = sigma)
+            Sigma.nn <- matern.covariance(h = as.matrix(dist(loc[nbrs])),
+                                          kappa = kappa, nu = nu, sigma = sigma)
+            N <- length(nbrs) + 1
+            tmp <- solve(Sigma.nn, Sigma.in)
+            val[counter + (1:N)] <- c(-t(tmp),1)
+            ii[counter + (1:N)] <- rep(i,N)
+            jj[counter + (1:N)] <- c(nbrs,i)
+            Fs.d[i] <- sigma^2 - t(Sigma.in)%*%tmp
+            Fsi.d[i] <- 1/Fs.d[i]    
+            counter <- counter + N
+        }
         
-        counter <- counter + N
+        
+        
         
     }
-    
+    ii <- ii[1:counter]
+    jj <- jj[1:counter]
+    val <- val[1:counter]
     Bs <-  Matrix::sparseMatrix(i   = ii,
                                 j    = jj,
                                 x    = val,
@@ -98,12 +192,39 @@ get.nn <- function(loc,kappa,nu,sigma, n.nbr) {
     return(list(Bs = Bs, Fs = Fs, Fi = Fsi))
 }
 
-get.nnQ <- function(loc,kappa,nu,sigma, n.nbr) {
-    tmp <- get.nn(loc,kappa,nu,sigma,n.nbr)
+get.nnQ <- function(loc,kappa,nu,sigma, n.nbr,S=NULL) {
+    tmp <- get.nn(loc,kappa,nu,sigma,n.nbr,S)
     
     return(t(tmp$Bs) %*% tmp$Fi %*%tmp$Bs)
 }
 
+
+get.nn.pred <- function(loc,kappa,nu,sigma, n.nbr, S = NULL) {
+    k <- length(loc)
+    N <- k*n.nbr
+    ii <- numeric(N)
+    jj <- numeric(N)
+    val <- numeric(N)
+    counter <- 0
+    for(i in 1:k) {
+        dists <- abs(loc[i] - loc[S])
+        nbrs <- sort(sort(dists, index.return = TRUE)$ix[1:n.nbr])
+        Sigma.in <- matern.covariance(h = abs(loc[S[nbrs]]-loc[i]),
+                                      kappa = kappa, nu = nu, sigma = sigma)
+        Sigma.nn <- matern.covariance(h = as.matrix(dist(loc[S[nbrs]])),
+                                      kappa = kappa, nu = nu, sigma = sigma)
+
+        val[counter + (1:n.nbr)] <- t(solve(Sigma.nn,Sigma.in))
+        ii[counter + (1:n.nbr)] <- rep(i,n.nbr)
+        jj[counter + (1:n.nbr)] <- nbrs
+        counter <- counter + n.nbr
+    }
+    Bs <-  Matrix::sparseMatrix(i   = ii,
+                                j    = jj,
+                                x    = val,
+                                dims = c(k, length(S)))
+    return(Bs)
+}
 ## Fourier 
 
 
--- aux_functions/nn_predict_compare.R
+++ aux_functions/nn_predict_compare.R
@@ -1,39 +1,44 @@
 library(rSPDE)
 source("aux_functions/aux_functions_cov.R")
-range = 1
+range = 2
 sigma = 2
 sigma.e <- 0.2
-n <- 1000
+n <- 2000
+n.obs <- 1000
 n.rep <- 1
 loc <- seq(0,n/100,length.out=n)
+
 D <- as.matrix(dist(loc))
 nu.vec <- seq(from = 0.1, to = 2.45, length.out = 10)
-m.vec <- 1:5
+m.vec <- 2:6
 t.nn <- t.rat <- t.nn1 <- t.rat1 <- err.nn <- err.rat <- matrix(0,nrow= length(nu.vec), ncol = length(m.vec))
 for(kk in 1:n.rep) {
+    obs.ind <- sort(sample(1:n)[1:n.obs])
     for(i in 1:length(nu.vec)) {
         cat(i/length(nu.vec),"\n")
         nu <- nu.vec[i]    
         alpha <- nu + 1/2
         kappa = sqrt(8*nu)/range
         Sigma <- matern.covariance(h=D,kappa=kappa,nu=nu,sigma=sigma)
-        R <- chol(Sigma)
-        X <- t(R)%*%rnorm(n)
-        Y <- X + sigma.e*rnorm(n)
-        Sigma.hat <- Sigma + sigma.e^2*diag(n)
-        mu <- Sigma%*%solve(Sigma.hat,Y)
+        R <- chol(Sigma[obs.ind,obs.ind])
+        X <- t(R)%*%rnorm(n.obs)
+        Y <- X + sigma.e*rnorm(n.obs)
+        Sigma.hat <- Sigma[obs.ind,obs.ind] + sigma.e^2*diag(n.obs)
+        mu <- Sigma[,obs.ind]%*%solve(Sigma.hat,Y)
         
         for(j in 1:length(m.vec)) { 
             m <- m.vec[j]
             
             #mn <- round(sqrt((m+1)^3*(ceil(alpha)+1)^2))
-            mn <- round(((m+2)*(ceil(alpha)+1)^(1.5)))
+            mn <- round(((m-1)*(ceil(alpha)+1)^(1.5)))
             
             t1 <- Sys.time()
-            Qnn <- get.nnQ(loc = loc,kappa = kappa,nu = nu,sigma = sigma, n.nbr = mn)
+            Qnn <- get.nnQ(loc = loc[obs.ind],kappa = kappa,nu = nu,sigma = sigma, n.nbr = mn)
             t2 <- Sys.time()
-            Qhat <- Qnn + Diagonal(n)/sigma.e^2        
+            Qhat <- Qnn + Diagonal(n.obs)/sigma.e^2        
             mu.nn <- solve(Qhat, Y/sigma.e^2)
+            Bp <- get.nn.pred(loc = loc, kappa = kappa, nu = nu, sigma = sigma, n.nbr = mn, S = obs.ind)
+            mu.nn <- Bp%*%mu.nn
             t3 <- Sys.time()
             t.nn[i,j] <- t.nn[i,j] + (t3-t2)/n.rep
             t.nn1[i,j] <- t.nn1[i,j] + (t2-t1)/n.rep
@@ -47,8 +52,8 @@ for(kk in 1:n.rep) {
             
             Q <- t(Qrat$L)%*%Qrat$D%*%Qrat$L
             t2 <- Sys.time()
-            Qhat.rat <- Q + t(Qrat$A)%*%Qrat$A/sigma.e^2        
-            mu.rat <- Qrat$A%*%solve(Qhat.rat, t(Qrat$A)%*%Y/sigma.e^2)
+            Qhat.rat <- Q + t(Qrat$A[obs.ind,])%*%Qrat$A[obs.ind,]/sigma.e^2        
+            mu.rat <- Qrat$A%*%solve(Qhat.rat, t(Qrat$A[obs.ind,])%*%Y/sigma.e^2)
             t3 <- Sys.time()
             t.rat[i,j] <- t.rat[i,j] + (t3-t2)/n.rep
             t.rat1[i,j] <- t.rat1[i,j] + (t3-t1)/n.rep
@@ -100,8 +105,8 @@ lines(nu.vec,t.nn1[,3], col = 3,lty=2)
 lines(nu.vec,t.nn1[,4], col = 4,lty=2)
 lines(nu.vec,t.nn1[,5], col = 5,lty=2)
 
-plot(nu.vec,t.rat1[,1]+t.rat[,1],type="l", log= "y", ylim = c(min(c(min(t.rat1),min(t.nn1))),
-                                                   max(c(max(t.rat1),max(t.nn1)))),
+plot(nu.vec,t.rat1[,1]+t.rat[,1],type="l", log= "y", ylim = c(min(c(min(t.rat1+t.rat),min(t.nn1+t.nn))),
+                                                   max(c(max(t.rat1+t.rat),max(t.nn1+t.nn)))),
      main = "time total")
 lines(nu.vec,t.rat1[,2]+t.rat[,2], col = 2)
 lines(nu.vec,t.rat1[,3]+t.rat[,3], col = 3)
