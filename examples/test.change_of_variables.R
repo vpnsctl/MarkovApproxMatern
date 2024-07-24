1ccc4580f95a5e32cda7c7eb888aada4f5e07863
David Bolin <bolinjd@kl-26412.kaust.edu.sa>
2024-07-24 14:29:10 +0300

    fix timing in test

--- examples/test.change_of_variables.R
+++ examples/test.change_of_variables.R
@@ -1,25 +1,4 @@
-rm(list=ls())
-library(Matrix)
-range = 2
-sigma = 2
-sigma.e <- 0.2
-n <- 10000
-n.obs <- 10000
-loc <- seq(0,n/100,length.out=n)
-
-obs.ind <- sort(sample(1:n)[1:n.obs])
-
-nu = 2.4
-alpha <- nu + 1/2
-kappa = sqrt(8*nu)/range
-#Sigma <- matern.covariance(h=D,kappa=kappa,nu=nu,sigma=sigma)
-#R <- chol(Sigma[obs.ind,obs.ind])
-#X <- t(R)%*%rnorm(n.obs)
-#Y <- X + sigma.e*rnorm(n.obs)
-Y <- matrix(rnorm(n.obs),n.obs,1)
-
-
-m = 1
+m = 8
 t1 <- Sys.time()
 Qrat <- rSPDE:::matern.rational.precision(loc = loc, order = m, nu = nu, kappa = kappa, 
                                           sigma = sigma, type_rational = "brasil", 
@@ -28,11 +7,11 @@ Q <- Qrat$Q
 t2 <- Sys.time()
 times.con1 <- t2 - t1
 
-t1 <- Sys.time()
+t3 <- Sys.time()
 Qhat.rat <- Q + t(Qrat$A[obs.ind,])%*%Qrat$A[obs.ind,]/sigma.e^2        
 mu.rat1 <- Qrat$A%*%solve(Qhat.rat, t(Qrat$A[obs.ind,])%*%Y/sigma.e^2)
-t2 <- Sys.time()
-times1 <- t2 - t1
+t4 <- Sys.time()
+times1 <- t4 - t3
 
 t1 <- Sys.time()
 Qrat <- rSPDE:::matern.rational.precision(loc = loc, order = m, nu = nu, kappa = kappa,
@@ -46,10 +25,9 @@ times.con2 <- t2 - t1
 
 t3 <- Sys.time()
 A2o <- A2[obs.ind,]
-t4 <- Sys.time()
 Qhat.rat2 <- Q2 + t(A2o)%*%A2o/sigma.e^2        
 mu.rat2 <- A2%*%solve(Qhat.rat2, t(A2o)%*%Y/sigma.e^2)
-t2 <- Sys.time()
+t4 <- Sys.time()
 times2 <- t4-t3
 
 res <- data.frame(Q.build = c(times.con1,times.con2),
