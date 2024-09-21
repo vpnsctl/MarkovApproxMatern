c272787ec051f4aaa0ec366409e8af9c8c6d3a40
vpnsctl <alexandre.impa@gmail.com>
2024-09-21 23:50:03 +0300

    update to save partial results

--- python_codes/gen_samples_and_true_pred.py
+++ python_codes/gen_samples_and_true_pred.py
@@ -8,8 +8,8 @@ import tensorflow_probability as tfp
 tf.random.set_seed(123)
 
 # Define your parameters
-n = 5000
-n_obs = 5000
+n = 10000
+n_obs = 10000
 n_rep = 100
 range_value = 2
 sigma = 1
@@ -100,8 +100,17 @@ def compute_true_mu(Sigma_row, obs_ind, sigma_e, Y):
 sim_data_result = np.zeros((tf.size(nu_vec), n_rep, n_obs))
 true_mean_result = np.zeros((tf.size(nu_vec), n_rep, n))
 
+sim_data_nu = np.zeros(n_rep, n_obs)
+true_mean_nu = np.zeros(n_rep, n)
+
 if(n != n_obs):
     obs_ind_result = np.zeros((tf.size(nu_vec), n_rep, n_obs))
+    obs_ind_nu = np.zeros(n_rep, n_obs)
+
+
+# Create directories for saving results
+output_dir = 'python_codes/partial_results'
+os.makedirs(output_dir, exist_ok=True)
 
 # Start timing the computations
 start_time = time.time()
@@ -125,10 +134,18 @@ for idx, nu in enumerate(nu_vec):
         sim_data = simulate_from_covariance(Sigma_row, obs_ind, sigma_e=sigma_e, R = R)
         mu = compute_true_mu(Sigma_row=Sigma_row, obs_ind=obs_ind, sigma_e=sigma_e, Y=sim_data)
 
-        sim_data_result[idx, i, :] = sim_data.numpy().flatten()
-        true_mean_result[idx, i, :] = mu.numpy().flatten()
+        sim_data_nu[i, :] = sim_data_result[idx, i, :] = sim_data.numpy().flatten()
+        true_mean_nu[i, :] = true_mean_result[idx, i, :] = mu.numpy().flatten()
         if(n != n_obs):
-            obs_ind_result[idx, i, :] = obs_ind.numpy().flatten()
+            obs_ind_nu[i, :] = obs_ind_result[idx, i, :] = obs_ind.numpy().flatten()        
+        
+        filename = f"{output_dir}/simulation_results_n{n}_nobs{n_obs}_range{range_value}_nu{nu.numpy():.2f}.h5"
+    
+        with h5py.File(filename, 'w') as f:
+            f.create_dataset('sim_data_nu', data=sim_data_nu)
+            f.create_dataset('true_mean_nu', data=true_mean_nu)
+            if(n != n_obs):
+                f.create_dataset('obs_ind_nu', data=obs_ind_nu)
 
     nu_elapsed_time = time.time() - nu_start_time
     print(f"Time for nu = {nu:.2f}: {nu_elapsed_time:.2f} seconds")
