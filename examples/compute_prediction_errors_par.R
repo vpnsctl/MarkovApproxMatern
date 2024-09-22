a4106472e843a731cc81af87b58c1a8ad9d08d1a
vpnsctl <alexandre.impa@gmail.com>
2024-09-22 13:28:21 +0300

    tables

--- examples/compute_prediction_errors_par.R
+++ examples/compute_prediction_errors_par.R
@@ -203,6 +203,36 @@ saveRDS(res_5000_pred, "pred_tables/res_5000_range1_rat_nngp.RDS")
 
 ## 
 
+
+rm(list=ls())
+source("aux_functions/aux_functions_cov.R")
+source("examples/error.computations.R")
+library(rSPDE)
+library(foreach)
+library(doParallel)
+library(doSNOW)
+
+cores=14
+
+cl <- makeCluster(cores[1]-1) 
+registerDoSNOW(cl)
+
+sigma = 1
+sigma.e <- 0.1
+
+
+nu.vec <- seq(from = 0.01, to = 2.49, by = 0.01)
+nu.vec <- nu.vec[length(nu.vec):1]
+m.vec <- 1:6
+
+iterations <- length(nu.vec)
+pb <- txtProgressBar(max = iterations, style = 3)
+progress <- function(n) setTxtProgressBar(pb, n)
+opts <- list(progress = progress)
+folder_to_save <- getwd()
+
+
+
 n <- 10000
 n.obs <- 10000
 n.rep <- 100
@@ -225,7 +255,7 @@ for(i in 1:length(res)) {
 
 res_10000_pred <- list(nu = nu, err.nn = err.nn, err.rat = err.rat)
 
-saveRDS(res_10000_pred, "pred_tables/res_10000_10000_range02_rat_nngp.RDS")
+saveRDS(res_10000_pred, "pred_tables/res_10000_10000_range5_rat_nngp.RDS")
 
 
 
