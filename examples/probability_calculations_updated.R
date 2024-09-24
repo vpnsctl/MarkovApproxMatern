b8936805e975e0ad1de7a4f096763e12e81a9c39
vpnsctl <alexandre.impa@gmail.com>
2024-09-24 11:19:23 +0300

    addjust + save partial results

--- examples/probability_calculations_updated.R
+++ examples/probability_calculations_updated.R
@@ -3,48 +3,21 @@ source("aux_functions/calibration_functions.R")
 
 m_nngp_fun <- function(m, alpha = NULL, n, m_vec = NULL){
     return(m_vec[m])
-
-    # if(alpha<1) {
-    #     mn <- m - 1
-    #     if(mn < 1){
-    #         mn <- 1
-    #     }
-    # } else if (alpha < 2) {
-    #     # if(n == 1000) {
-    #     m_vec = c(1, 2, 6, 14, 20, 24)
-    #     # } else if(n == 5000){
-    #     #     m_vec <- c(1, 2, 13, 21, 27, 31)
-    #     # } else if(n.obs == 5000){
-    #     #     m_vec <- c(1, 2, 3, 4, 14, 20)
-    #     # } else if(n.obs == 10000){
-    #     #     m_vec <- c(1, 2, 7, 18, 24, 29)
-    #     # } else{
-    #     #     stop("not implemented")
-    #     # }
-    #     mn <- m_vec[m]
-    # } else {
-    #     # if(n == 1000){
-    #     m_vec <- c(4, 17, 24, 29, 32, 36) 
-    #     # } else if(n == 5000){
-    #     #     m_vec <- c(15, 30, 37, 45, 51, 54)
-    #     # } else if(n.obs == 5000){
-    #     #     m_vec <- c(1, 22, 31, 39, 47, 54)
-    #     # } else if(n.obs == 10000){
-    #     #     m_vec <- c(14, 28, 37, 44, 51, 57)
-    #     # } else{
-    #     #     stop("not implemented")
-    #     # }
-    #     mn <- m_vec[m]
-    # }
-    # return(mn)
 }
-
 
 source("aux_functions/aux_functions_cov.R")
 source("aux_functions/probability_computations.R")
 library(rSPDE)
 library(excursions)
 library(mvtnorm)
+
+# Create directories for saving results
+if (!dir.exists("prob_tables")) {
+    dir.create("prob_tables")
+}
+if (!dir.exists("prob_tables/partial_results")) {
+    dir.create("prob_tables/partial_results")
+}
 
 use.excursions <- TRUE
 folder_to_save <- "markov_approx"
@@ -53,39 +26,41 @@ range = 1
 sigma = 1
 sigma.e <- 0.1
 n <- c(0, 25, 50, 100, 150, 250,  300, 400, 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000)
+n <- n[length(n):1]
 n.obs <- 250
 n.rep <- 10
 max_it_per_m <- 20 # for calibration
 samples_calibration <- 50
-nu <- 1.9 - 1/2
+nu <- 1.9 - 1/2 # 0.4, 1, 2 -> alpha = 0.9, 1.5 and 2.5
 alpha <- nu + 1/2
 coverage <- 0.9
 m_vec = 1:6   # rational order
 mn_vec <- rep(0,length(m_vec))
+domain_upper_limit <- 10
 
 prob_nngp <- prob_true <- prob_rat <- matrix(0, nrow = length(n), ncol = n.rep)
 err_nngp <- err_rat <- matrix(0, length(m_vec), length(n))
 
 locs <- list()
 for(i in 1:length(n)) {
-    locs[[i]] = seq(from=1,to=25,length.out = n[i])
+    locs[[i]] = seq(from=1,to=domain_upper_limit,length.out = n[i])
 }
 
 kappa = sqrt(8*nu)/range
 
 previous_calibration <- NULL
 
+calibrated_m <- list()
+
 for(j in 1:n.rep){
-    obs_loc <- sort(25*runif(n.obs))
+    obs_loc <- sort(domain_upper_limit*runif(n.obs))
     obs_cov <- get_cov_mat(loc = obs_loc, m = NULL, method = "true", nu = nu, kappa = kappa, sigma = sigma, 
                             samples = NULL, L=NULL)
     z <- matrix(rnorm(n.obs), ncol = n.obs, nrow = 1)
     L <- chol(obs_cov)
     y <- t(L)%*%t(z) + sigma.e * rnorm(n.obs)
     
-    
     for(i in 1:length(n)) {
-        # add mesh and observation locations and sort them
         if(n[i]>0){ 
             loc <- c(obs_loc, locs[[i]])
             obs.ind <- 1:length(loc)
@@ -99,15 +74,18 @@ for(j in 1:n.rep){
             obs.ind <- 1:n.obs
         }
 
-        print("Calibrating...")
-        # Get calibration
-        previous_calibration <- auto_calibration_nngp_rat(n=n[i]+n.obs, n_obs=n.obs, nu=nu, range=range, sigma=sigma, sigma_e=sigma.e, 
+        if(is.null(calibrated_m[[as.character(n[i])]])){
+            if(n[i] > 999){
+                print("Calibrating...")
+                previous_calibration <- auto_calibration_nngp_rat(n=n[i]+n.obs, n_obs=n.obs, nu=nu, range=range, sigma=sigma, sigma_e=sigma.e, 
                     samples=samples_calibration, m_rat=m_vec, previous_calibration = previous_calibration, max_it_per_m = max_it_per_m, print=FALSE)
+                calibrated_m[[as.character(n[i])]] <-  previous_calibration                   
+                cat("Calibration:", previous_calibration, "\n")
+            } else{
+                calibrated_m[[as.character(n[i])]] <- calibrated_m[[as.character(1000)]]
+            }
+        } 
         
-        print(paste("Calibration:", previous_calibration))
-        
-        
-        ## Compute credible band and true probabilitiy
         cat("rep = ", j,"n =", n[i], 'compute truth\n')
         
         true_cov <- get_cov_mat(loc = loc, m = NULL, method = "true", nu = nu, kappa = kappa, sigma = sigma, 
@@ -128,7 +106,6 @@ for(j in 1:n.rep){
         
         for(k in 1:length(m_vec)){
             m <- m_vec[k]
-            ## Rational approx
             cat("rep = ", j,"n =", n[i], 'rational, m = ', m, '\n')
             
             Qrat <-rSPDE:::matern.rational.precision(loc = loc, order = m, nu = nu, kappa = kappa, sigma = sigma, 
@@ -151,10 +128,7 @@ for(j in 1:n.rep){
             
             cat("prob rat: ", prob_rat, '\n')
             
-            # nngp
-            
-            mn <- previous_calibration[k]
-            # mn <- m_nngp_fun(m, alpha, n[i])
+            mn <- calibrated_m[[as.character(n[i])]][k]
             cat("rep = ", j,"n =", n[i], 'nngp, m = ', mn, '\n')
             
             prec_nngp <- get_cov_mat(loc = loc[obs.ind], m = mn, method = "nngp", 
@@ -176,24 +150,21 @@ for(j in 1:n.rep){
             
             cat("rat: ", err_rat[k,i]/j, '\n')
             cat("nngp: ", err_nngp[k,i]/j, '\n')
+
+            # Save partial results
+            partial_res_list <- list(
+                err_rat = err_rat[,i],
+                err_nngp = err_nngp[,i],
+                n = n[i],
+                rep = j,
+                m = m_vec
+            )
+            saveRDS(partial_res_list, file = paste0("prob_tables/partial_results/partial_result_rep", j, "_n", n[i], "_range",range,"_nu",nu,".RDS"))
         }
     }
 }
 err_rat <- err_rat/n.rep
 err_nngp <- err_nngp/n.rep
 
-res_list <- list(err_rat = err_rat, err_nngp = err_nngp, nu = nu, range = range)
-
-saveRDS(res_list, paste0("prob_errors_range",range,"_nu",nu,".RDS"))
-
-
-plot(n,abs(err_rat[1,]), type="l", ylim = c(0.00001,1), log="y", ylab = "Absolute error")
-lines(n,abs(err_rat[2,]),col=2)
-lines(n,abs(err_rat[3,]),col=3)
-lines(n,abs(err_rat[4,]),col=4)
-lines(n,abs(err_rat[5,]),col=5)
-lines(n,abs(err_nngp[1,]),col=1, lty=2)
-lines(n,abs(err_nngp[2,]),col=2, lty=2)
-lines(n,abs(err_nngp[3,]),col=3, lty=2)
-lines(n,abs(err_nngp[4,]),col=4, lty=2)
-lines(n,abs(err_nngp[5,]),col=5, lty=2)
+res_list <- list(err_rat = err_rat, err_nngp = err_nngp, nu = nu, m_vec = m_vec, n_vec = n)
+saveRDS(res_list, file = "prob_tables/rat_vs_nngp_range",range,"_nu",nu,".RDS")
