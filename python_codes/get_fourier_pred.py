4152ed889093b65bd9b7c53288c445b0c48c6b3b
vpnsctl <alexandre.impa@gmail.com>
2024-09-28 00:01:50 +0300

    more tables

--- python_codes/get_fourier_pred.py
+++ python_codes/get_fourier_pred.py
@@ -199,7 +199,7 @@ def get_fourier_errors(n, n_obs, range_val, n_rep, sigma, sigma_e, samples_fouri
 
 n = 10000
 n_obs = 10000
-range_val = 1
+range_val = 2
 sigma = 1.0
 sigma_e = 0.1
 folder_to_save = os.getcwd()
