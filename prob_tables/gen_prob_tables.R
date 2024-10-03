43eb8d9b1083d3fad01f3129abdac9879ddca010
vpnsctl <alexandre.impa@gmail.com>
2024-10-03 12:37:44 +0300

    all tables processed

---
+++ prob_tables/gen_prob_tables.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+
+prob_df <- data.frame()
+
+prob_tmp <- readRDS("prob_tables/full_results/rat_vs_nngp_range0.5_nu1.RDS")
+
+prob_df <- prob_tmp$err_rat
+colnames(prob_df) <- prob_tmp$n_vec
+rownames(prob_df) <- prob_tmp$m_vec
+
+prob_df <- as.data.frame(prob_df) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df[["Method"]] <- "Rational"
+prob_df[["range"]] <- "0.5"
+
+prob_df_tmp <- prob_tmp$err_nngp
+colnames(prob_df_tmp) <- prob_tmp$n_vec
+rownames(prob_df_tmp) <- prob_tmp$m_vec
+
+prob_df_tmp <- as.data.frame(prob_df_tmp) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df_tmp[["Method"]] <- "nnGP"
+prob_df_tmp[["range"]] <- "0.5"
+
+prob_df <- bind_rows(prob_df, prob_df_tmp)
+
+prob_tmp <- readRDS("prob_tables/full_results/rat_vs_nngp_range1_nu1.RDS")
+
+prob_df_tmp <- prob_tmp$err_rat
+colnames(prob_df_tmp) <- prob_tmp$n_vec
+rownames(prob_df_tmp) <- prob_tmp$m_vec
+
+prob_df_tmp <- as.data.frame(prob_df_tmp) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df_tmp[["Method"]] <- "Rational"
+prob_df_tmp[["range"]] <- "1"
+
+prob_df <- bind_rows(prob_df, prob_df_tmp)
+
+prob_df_tmp <- prob_tmp$err_nngp
+colnames(prob_df_tmp) <- prob_tmp$n_vec
+rownames(prob_df_tmp) <- prob_tmp$m_vec
+
+prob_df_tmp <- as.data.frame(prob_df_tmp) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df_tmp[["Method"]] <- "nnGP"
+prob_df_tmp[["range"]] <- "1"
+
+prob_df <- bind_rows(prob_df, prob_df_tmp)
+
+prob_tmp <- readRDS("prob_tables/full_results/rat_vs_nngp_range2_nu1.RDS")
+
+prob_df_tmp <- prob_tmp$err_rat
+colnames(prob_df_tmp) <- prob_tmp$n_vec
+rownames(prob_df_tmp) <- prob_tmp$m_vec
+
+prob_df_tmp <- as.data.frame(prob_df_tmp) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df_tmp[["Method"]] <- "Rational"
+prob_df_tmp[["range"]] <- "2"
+
+prob_df_tmp <- prob_tmp$err_nngp
+colnames(prob_df_tmp) <- prob_tmp$n_vec
+rownames(prob_df_tmp) <- prob_tmp$m_vec
+
+prob_df_tmp <- as.data.frame(prob_df_tmp) %>%
+  rownames_to_column(var = "m") %>%  
+  pivot_longer(-m, names_to = "N", values_to = "prob_error")  
+
+prob_df_tmp[["Method"]] <- "nnGP"
+prob_df_tmp[["range"]] <- "2"
+
+prob_df <- bind_rows(prob_df, prob_df_tmp)
+
+prob_df <- bind_rows(prob_df, prob_df_tmp)
+
+saveRDS(prob_df, "prob_tables/prob_errors.RDS")
