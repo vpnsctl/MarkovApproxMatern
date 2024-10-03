1aa361e7e8540aea0c1aace0cddd60e4cea22297
vpnsctl <alexandre.impa@gmail.com>
2024-10-03 14:08:16 +0300

    updates

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -2,21 +2,19 @@ library(shiny)
 library(dplyr)
 library(tidyr)
 
-dist_range_02 <- readRDS("../distance_tables/dist_range_02.RDS")
-dist_range_05 <- readRDS("../distance_tables/dist_range_05.RDS")
-dist_range_1 <- readRDS("../distance_tables/dist_range_1.RDS")
+dist_df <- readRDS("../distance_tables/full_dists.RDS")
+pred_df <- readRDS("../pred_tables/pred_error.RDS")
+prob_df <- readRDS("../prob_tables/prob_errors.RDS")
 
-dist_range_02_samp <- readRDS("../distance_tables/dist_range_02_samp.RDS")
-dist_range_05_samp <- readRDS("../distance_tables/dist_range_05_samp.RDS")
-dist_range_1_samp <- readRDS("../distance_tables/dist_range_1_samp.RDS")
+pred_df <- pred_df |> rename(Error = pred_error)
+prob_df <- prob_df |> rename(Error = prob_error)
 
-pred_range02 <- readRDS("../distance_tables/pred_range02.RDS")
+prob_df <- prob_df |> mutate(Error = abs(Error))
 
 
 server <- function(input, output, session) {
   
   observe({
-    which_calib <- input$whichCalib
     range_val <- input$rangeParameter
     approxNorm <- input$approxNorm
     plotStyle <- input$plotStyle
@@ -26,35 +24,20 @@ server <- function(input, output, session) {
     m_order <- input$orderRat
     methods_approx <- input$methods_approx
     methods_approx <- c("Rational", methods_approx)
-    numberLoc <- input$numberLoc
+    numberLoc <- as.numeric(input$numberLoc)
+    numberObs <-numberLoc
     nuRange <- input$nuRange
-    
-    if(which_calib == "Sampling"){
-      error_02 <- dist_range_02_samp
-      error_05 <- dist_range_05_samp
-      error_1 <- dist_range_1_samp
-    } else{
-      error_02 <- dist_range_02
-      error_05 <- dist_range_05
-      error_1 <- dist_range_1
-    }
-    
+        
     color_plot_options <- c("black", "steelblue", 
                     "limegreen", "red",
                     "purple","orange","pink")
     
-    if(range_val == "0.2"){
-      error_table <- error_02
-    } else if(range_val == "0.5"){
-      error_table <- error_05
-    } else{
-      error_table <- error_1
-    }
-    
+    error_table <- dist_df |> dplyr::filter(range == range_val)
+        
     if(!is.null(m_order)){
       color_plot_used <- color_plot_options[as.numeric(m_order)+1]
       
-      error_table <- error_table %>% dplyr::filter(m %in% as.character(m_order), Dist == approxNorm, nu <= nuRange[2], nu >= nuRange[1], N == numberLoc)
+      error_table <- error_table %>% dplyr::filter(m %in% as.character(m_order), Dist == approxNorm, nu <= nuRange[2], nu >= nuRange[1], N == numberLoc, n_obs == numberObs)
 
       idx_table <- (error_table[["Method"]]%in%methods_approx)
 
@@ -111,31 +94,24 @@ server <- function(input, output, session) {
     range_val_pred <- input$rangeParameter_pred
     approxNorm_pred <- input$approxNorm_pred
     plotStyle_pred <- input$plotStyle_pred
-    if(approxNorm_pred == "Max"){
-      approxNorm_pred <- "max"
-    }
     m_order_pred <- input$orderRat_pred
     methods_approx_pred <- input$methods_approx_pred
     methods_approx_pred <- c("Rational_LDL", methods_approx_pred)
     numberLoc_pred <- input$numberLoc_pred
     nuRange_pred <- input$nuRange_pred
+    numberObs_pred <- min(as.numeric(input$numberObs_pred), as.numeric(input$numberLoc_pred))
+    methods_approx_pred <- c("Rational", methods_approx_pred)
     
     color_plot_options <- c("black", "steelblue", 
                     "limegreen", "red",
                     "purple","orange","pink")
     
-    if(range_val_pred == "0.2"){
-      pred_table <- pred_range02
-    } else if(range_val_pred == "0.5"){
-      pred_table <- pred_range05
-    } else{
-      pred_table <- pred_range1
-    }
+    pred_table <- pred_df |> dplyr::filter(range == range_val_pred)
     
     if(!is.null(m_order_pred)){
       color_plot_used <- color_plot_options[as.numeric(m_order_pred)+1]
       
-      pred_table <- pred_table %>% dplyr::filter(m %in% as.character(m_order_pred), Norm == approxNorm_pred, nu <= nuRange_pred[2], nu >= nuRange_pred[1], N == numberLoc_pred)
+      pred_table <- pred_table %>% dplyr::filter(m %in% as.character(m_order_pred), nu <= nuRange_pred[2], nu >= nuRange_pred[1], N == numberLoc_pred, n_obs == numberObs_pred)
 
       idx_table <- (pred_table[["Method"]]%in%methods_approx_pred)
 
@@ -148,7 +124,7 @@ server <- function(input, output, session) {
         fig <- ggplot(pred_table, aes(x = nu, y = Error, linetype=m, color = Method)) + geom_line() 
       }
             
-      y_label_cov <- ifelse(approxNorm_pred == "l2", "Error in l2-norm", "Error in Max-norm")
+      y_label_cov <- "Error in l2-norm"
       
       fig <- fig +labs(y= y_label_cov, x = "\u028b (smoothness parameter)")
       
@@ -156,14 +132,14 @@ server <- function(input, output, session) {
         fig <- fig + scale_y_log10()
       }
       
-            output$downloadPlotCov <- downloadHandler(
+            output$downloadPlotPred <- downloadHandler(
           filename = function() { paste0("Pred_error_plot.", input$fileExtensionCov) },
           content = function(file) {
             ggsave(file, plot = fig, device = input$fileExtensionCov)
           }
         )
         
-        output$downloadDataFrameCov <- downloadHandler(
+        output$downloadDataFramePred <- downloadHandler(
           filename = function() { paste0("Pred_error_data_frame_range_",range_val,".RDS") },
           content = function(file) {
             saveRDS(error_table, file = file)
@@ -179,6 +155,73 @@ server <- function(input, output, session) {
     }
     
     output$prederrors <- renderPlotly({
+      
+      fig_plotly
+      
+    })
+   })
+
+  
+   observe({
+    range_val_prob <- input$rangeParameter_prob
+    approxNorm_prob <- input$approxNorm_prob
+    plotStyle_prob <- input$plotStyle_prob
+    m_order_prob <- input$orderRat_prob
+    nuRange_prob <- 1
+     
+    color_plot_options <- c("black", "steelblue", 
+                    "limegreen", "red",
+                    "purple","orange","pink")
+    
+    prob_table <- prob_df |> dplyr::filter(range == range_val_prob)
+     
+    prob_table[["N"]] <- as.numeric(prob_table[["N"]])
+    
+    if(!is.null(m_order_prob)){
+      color_plot_used <- color_plot_options[as.numeric(m_order_prob)+1]
+      
+      prob_table <- prob_table %>% dplyr::filter(m %in% as.character(m_order_prob))
+
+      print(prob_table)
+      
+      if(plotStyle_prob == "Linetype (Method), color (Order)"){
+        fig <- ggplot(prob_table, aes(x = N, y = Error, linetype=Method, color = m)) + geom_line() +
+          scale_color_manual(values = color_plot_used)
+      } else{
+        fig <- ggplot(prob_table, aes(x = N, y = Error, linetype=m, color = Method)) + geom_line() 
+      }
+            
+      y_label_cov <- "Probability error"
+      
+      fig <- fig +labs(y= y_label_cov, x = "N")
+      
+      if(input$logScaleCoverror){
+        fig <- fig + scale_y_log10()
+      }
+      
+            output$downloadPlotprob <- downloadHandler(
+          filename = function() { paste0("Prob_error_plot.", input$fileExtensionCov) },
+          content = function(file) {
+            ggsave(file, plot = fig, device = input$fileExtensionCov)
+          }
+        )
+        
+        output$downloadDataFrameprob <- downloadHandler(
+          filename = function() { paste0("Prob_error_data_frame_range_",range_val,".RDS") },
+          content = function(file) {
+            saveRDS(error_table, file = file)
+          }
+        )
+      
+      fig_plotly <- ggplotly(fig,height = 755)
+      
+      
+    } else{
+      m_order = c(2)
+      updateCheckboxGroupInput(session, "orderRat_prob", selected= c("2"))
+    }
+    
+    output$proberrors <- renderPlotly({
       
       fig_plotly
       
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -3,105 +3,129 @@ library(plotly)
 library(shinythemes)
 library(shinyWidgets)
 
-
-dist_range_02 <- readRDS("../distance_tables/dist_range_02.RDS")
-
-nu_val <- sort(unique(dist_range_02[["nu"]]))
-
+nu_val <- seq(0.01, 2.49, 0.01)
 
 ui <- navbarPage(
-  "Markov Rational Approxiomation",
+  "Markov Rational Approximation",
+  
   tabPanel("Covariance error",
            sidebarLayout(position = "left",
                          sidebarPanel(width = 3,
-                                      radioButtons(inputId = "whichCalib", label = "Which calibration?",
-                                                         choices = c("Sampling", "Prediction"), 
-                                                         selected = "Sampling",
-                                                         inline=TRUE),
                                       checkboxGroupInput(inputId = "methods_approx", label = "Methods to be included",
                                                          choices = c("State-Space", "nnGP", "Fourier", "PCA"), 
                                                          selected = "State-Space",
-                                                         inline=TRUE),                                                         
-                                      checkboxGroupInput(inputId = "orderRat", label = "Order of the approximation",choices = 0:6, selected = 0:6,
-                                                         inline=TRUE),
+                                                         inline = TRUE),                                                         
+                                      checkboxGroupInput(inputId = "orderRat", label = "Order of the approximation",
+                                                         choices = 0:6, selected = 0:6,
+                                                         inline = TRUE),
                                       radioButtons(inputId = "numberLoc", label = "Number of locations",
-                                                   choices = c("500", "1000"),
-                                                   selected = "1000",
-                                                   inline=TRUE),
+                                                   choices = c("5000", "10000"),
+                                                   selected = "10000",
+                                                   inline = TRUE),                                           
                                       radioButtons(inputId = "rangeParameter", label = "Range Parameter",
-                                                   choices = c("0.2", "0.5", "1"),
-                                                   selected = "0.2",
-                                                   inline=TRUE),
+                                                   choices = c("0.5", "1", "2"),
+                                                   selected = "0.5",
+                                                   inline = TRUE),
                                       radioButtons(inputId = "approxNorm", label = "Which norm?",
                                                    choices = c("L2", "Sup"),
                                                    selected = "L2",
-                                                   inline=TRUE),
+                                                   inline = TRUE),
                                       sliderTextInput(inputId = "nuRange", label = "Which values of nu?",
-                                                   choices = nu_val,
-                                                   selected = range(nu_val)),
+                                                       choices = nu_val,
+                                                       selected = range(nu_val)),
                                       radioButtons(inputId = "plotStyle", label = "Which style?",
                                                    choices = c("Linetype (Method), color (Order)", "Linetype (Order), color (Method)"),
                                                    selected = "Linetype (Method), color (Order)",
-                                                   inline=FALSE),                                                   
+                                                   inline = FALSE),                                                   
                                       checkboxInput("logScaleCoverror", "Use log scale?", value = TRUE),
                                       downloadButton('downloadPlotCov', 'Download Plot'),
                                       radioButtons(inputId = "fileExtensionCov", label = "File extension",
                                                    choices = c("png", "pdf"),
                                                    selected = "png",
-                                                   inline=TRUE),
-                                      downloadButton('downloadDataFrameCov', 'Download Data Frame'),),
+                                                   inline = TRUE),
+                                      downloadButton('downloadDataFrameCov', 'Download Data Frame')
+                         ),  # Removed extra comma here
                          mainPanel(
-                           width = 12 - 3,
+                           width = 9,  # Set to 9 for clarity
                            plotlyOutput("raterrors")
                          )
-           ),
-           
+           )
   ),
   
-  tabPanel("Prediction error", sidebarLayout(position = "left",
-                                             sidebarPanel(width = 3,
+  tabPanel("Prediction error", 
+           sidebarLayout(position = "left",
+                         sidebarPanel(width = 3,
                                       checkboxGroupInput(inputId = "methods_approx_pred", label = "Methods to be included",
                                                          choices = c("State-Space", "nnGP", "Fourier", "PCA"), 
                                                          selected = "State-Space",
-                                                         inline=TRUE),                                                         
-                                      checkboxGroupInput(inputId = "orderRat_pred", label = "Order of the approximation",choices = 0:6, selected = 0:6,
-                                                         inline=TRUE),
+                                                         inline = TRUE),                                                         
+                                      checkboxGroupInput(inputId = "orderRat_pred", label = "Order of the approximation",
+                                                         choices = 0:6, selected = 0:6,
+                                                         inline = TRUE),
                                       radioButtons(inputId = "numberLoc_pred", label = "Number of locations",
-                                                   choices = c("500", "1000"),
-                                                   selected = "1000",
-                                                   inline=TRUE),
+                                                   choices = c("5000", "10000"),
+                                                   selected = "10000",
+                                                   inline = TRUE),
+                                      radioButtons(inputId = "numberObs_pred", label = "Number of observations",
+                                                   choices = c("5000", "10000"),
+                                                   selected = "10000",
+                                                   inline = TRUE),                                                   
                                       radioButtons(inputId = "rangeParameter_pred", label = "Range Parameter",
-                                                   choices = c("0.2", "0.5", "1"),
-                                                   selected = "0.2",
-                                                   inline=TRUE),
-                                      radioButtons(inputId = "approxNorm_pred", label = "Which norm?",
-                                                   choices = c("l2", "Max"),
-                                                   selected = "l2",
-                                                   inline=TRUE),
+                                                   choices = c("0.5", "1", "2"),
+                                                   selected = "0.5",
+                                                   inline = TRUE),
                                       sliderTextInput(inputId = "nuRange_pred", label = "Which values of nu?",
-                                                   choices = nu_val,
-                                                   selected = range(nu_val)),
+                                                       choices = nu_val,
+                                                       selected = range(nu_val)),
                                       radioButtons(inputId = "plotStyle_pred", label = "Which style?",
                                                    choices = c("Linetype (Method), color (Order)", "Linetype (Order), color (Method)"),
                                                    selected = "Linetype (Method), color (Order)",
-                                                   inline=FALSE),  
+                                                   inline = FALSE),  
                                       checkboxInput("logScalepredError", "Use log scale?", value = TRUE),
                                       downloadButton('downloadPlotPred', 'Download Plot'),
                                       radioButtons(inputId = "fileExtensionPred", label = "File extension",
                                                    choices = c("png", "pdf"),
                                                    selected = "png",
-                                                   inline=TRUE),
-                                      downloadButton('downloadDataFramePred', 'Download Data Frame'),),
-                                             
-                                             mainPanel(
-                                               width = 12 - 3,
-                                               plotlyOutput("prederrors")
-                                               
-                                             )
-  ),),
+                                                   inline = TRUE),
+                                      downloadButton('downloadDataFramePred', 'Download Data Frame')
+                         ),  # Removed extra comma here
+                         mainPanel(
+                           width = 9,  # Set to 9 for clarity
+                           plotlyOutput("prederrors")
+                         )
+           )
+  ),
   
+  tabPanel("Posterior probability error", 
+           sidebarLayout(position = "left",
+                         sidebarPanel(width = 3,     
+                                      checkboxGroupInput(inputId = "orderRat_prob", label = "Order of the approximation",
+                                                         choices = 1:6, selected = 1:6,
+                                                         inline = TRUE),                                                
+                                      radioButtons(inputId = "rangeParameter_prob", label = "Range Parameter",
+                                                   choices = c("0.5", "1", "2"),
+                                                   selected = "0.5",
+                                                   inline = TRUE),
+                                      radioButtons(inputId = "plotStyle_prob", label = "Which style?",
+                                                   choices = c("Linetype (Method), color (Order)", "Linetype (Order), color (Method)"),
+                                                   selected = "Linetype (Method), color (Order)",
+                                                   inline = FALSE),  
+                                      checkboxInput("logScaleprobError", "Use log scale?", value = TRUE),
+                                      downloadButton('downloadPlotprob', 'Download Plot'),
+                                      radioButtons(inputId = "fileExtensionprob", label = "File extension",
+                                                   choices = c("png", "pdf"),
+                                                   selected = "png",
+                                                   inline = TRUE),
+                                      downloadButton('downloadDataFrameprob', 'Download Data Frame')
+                         ),  # Removed extra comma here
+                         mainPanel(
+                           width = 9,  # Set to 9 for clarity
+                           plotlyOutput("proberrors")
+                         )
+           )
+  ),
+
   tags$style(type = "text/css", "body {padding-top: 70px;}"),
   theme = shinytheme("cosmo"),
   position = "fixed-top"
-  
 )
