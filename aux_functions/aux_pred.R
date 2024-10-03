f590027dd23f950dbb5132633ebb088f06232212
vpnsctl <alexandre.impa@gmail.com>
2024-10-03 12:23:55 +0300

    pred errors

--- aux_functions/aux_pred.R
+++ aux_functions/aux_pred.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(readr)
 
 extract_pred_params_from_filename <- function(filename) {
     file_base <- tools::file_path_sans_ext(basename(filename))
@@ -12,6 +13,15 @@ extract_pred_params_from_filename <- function(filename) {
         n_obs <- as.integer(match[[1]][3])
         range <- as.numeric(match[[1]][4])
         method <- match[[1]][5]
+        method <- switch(
+          method,
+          "fourier" = "Fourier",
+          "statespace" = "State-Space",
+          "rational" = "Rational",
+          "nngp" = "nnGP",
+          "pca" = "PCA",
+          stop("Unknown method: ", method)
+        )     
         
         return(list(N = N, n_obs = n_obs, range = range, method = method))
     } else {
@@ -25,29 +35,34 @@ process_pred_files <- function(file_paths) {
 
   for (file in file_paths) {
       params <- extract_pred_params_from_filename(file)
-    print(params)
       N <- params$N
       n_obs <- params$n_obs
       range_val <- params$range
+      method <- params$method
 
       if (grepl(".RDS$", file)) {
           pred_data <- readRDS(file)
+          pred_data <- pred_data[[grep("^err\\.", names(pred_data))]]
+          colnames(pred_data) <- c(1:6, "nu")
       } else if (grepl(".csv$", file)) {
-          pred_data <- read_csv(file)
+          pred_data <- read.csv(file)
+          colnames(pred_data) <- c("nu", 1:6)
       }
-    
-      pred_data <- tibble(data)
-      colnames(pred_data) <- c(1:6, "nu")
+      pred_data <- tibble(pred_data)
       pred_data[["range"]] <-  range_val
       pred_data[["n_obs"]] <- n_obs
       pred_data[["N"]] <- N
+      pred_data[["Method"]] <- method
       all_dfs[[file]] <- pred_data
   }
   
-  all_dfs <- all_dfs
-
-  combined_df <- process_dist(all_dfs)
-  combined_df <- unique(combined_df)
-  
-  return(combined_df)
+  all_dfs <- do.call(bind_rows,all_dfs)
+  all_dfs <- all_dfs |>
+    pivot_longer(
+      cols = `1`:`6`,  
+      names_to = "m",  
+      values_to = "pred_error"  
+    )
+    
+  return(all_dfs)
 }
--- pred_tables/gen_pred_tables.R
+++ pred_tables/gen_pred_tables.R
@@ -2,4 +2,5 @@ source("aux_functions/aux_pred.R")
 
 files <- list.files("pred_tables/full_tables", full.names = TRUE)
 
-df_dist <- process_pred_files(files)
+pred_error <- process_pred_files(files)
+saveRDS(pred_error, "pred_tables/pred_error.RDS")
