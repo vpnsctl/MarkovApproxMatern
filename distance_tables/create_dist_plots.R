c12a080a9a7eea674c84faed6fabc1009fca6ac7
vpnsctl <alexandre.impa@gmail.com>
2024-10-12 01:11:03 +0300

    more adjusts

--- distance_tables/create_dist_plots.R
+++ distance_tables/create_dist_plots.R
@@ -36,7 +36,7 @@ df_filtered$Facet_Cols <- factor(df_filtered$Facet_Cols,
   labels = c("Rational", expression(Order == 3), expression(Order == 5)))
 
 
-markers <- c("Rational" = NA, "nnGP" = 0, "State-Space" = 1, "Fourier" = 2, "PCA" = NA)
+# markers <- c("Rational" = NA, "nnGP" = 0, "State-Space" = 1, "Fourier" = 2, "PCA" = NA)
 
 df_filtered <- df_filtered |> 
   mutate(Dist = factor(Dist, levels = c("L2", "Linf"), 
@@ -47,22 +47,25 @@ df_points <- df_filtered |> arrange(Order) |> slice(seq(1, n(), by = 30))
 
 df_filtered$Order <- factor(df_filtered$Order)
 
-df_filtered$Method <- factor(df_filtered$Method, levels = c("Rational", "PCA", "Fourier", "nnGP", "State-Space"))
+df_filtered$Method <- factor(df_filtered$Method, levels = c("Rational", "PCA", "Fourier", "nnGP", "State-Space", "FEM", "Taper"))
 
 line_types <- c("Rational" = "solid", 
                 "nnGP" = "11", 
                 "State-Space" = "81", 
                 "Fourier" = "solid", 
-                "PCA" = "dashed")
+                "PCA" = "dashed",
+                "FEM" = "twodash",
+                "Taper" = 3313)
 
-line_sizes <- c("Rational" = 0.7, "nnGP" = 0.7, "State-Space" = 0.7, "Fourier" = 0.45, "PCA" = 0.7)
+
+line_sizes <- c("Rational" = 0.7, "nnGP" = 0.7, "State-Space" = 0.7, "Fourier" = 0.45, "PCA" = 0.7, "FEM" = 0.7, "Taper" = 0.7)
 
 p <- ggplot(df_filtered, aes(x = nu, y = Error, color = Order, linetype = Method, size = Method)) +
   geom_line(aes(size = ifelse(Method == "Fourier", 0.45, 0.7))) + 
   # geom_point(data = df_points, size = 3) +
   scale_y_log10(limits = c(1e-10, NA)) +
   scale_color_manual(values = color_plot_used, guide = guide_legend(order = 2)) +
-  scale_shape_manual(values = markers, guide = guide_legend(order = 1)) +
+  # scale_shape_manual(values = markers, guide = guide_legend(order = 1)) +
   scale_size_manual(values = line_sizes) +
   scale_linetype_manual(values = line_types, , guide = guide_legend(order = 1)) +  
   labs(y = "Covariance Error", x = expression(nu ~ "(smoothness parameter)")) +
@@ -87,12 +90,20 @@ p <- ggplot(df_filtered, aes(x = nu, y = Error, color = Order, linetype = Method
         strip.placement = "outside",
         strip.text.y = element_text(size = 14, face = "bold"),
         legend.box = "horizontal",  
-    legend.box.just = "center",  
-    legend.spacing.x = unit(0.5, "cm"),  
-    legend.margin = margin(t = 0, b = 0),  
-    legend.title.align = 0.5  
-      ) +
-          guides(linetype = guide_legend(order = 1, override.aes = list(size = 16, linewidth = c(0.8,0.8,0.4,0.8,0.8))), color = guide_legend(override.aes = list(size = 1)),size = "none")
+        legend.box.just = "center", 
+        legend.spacing.x = unit(0.5, "cm"),  
+        legend.margin = margin(t = 0, b = 0), 
+        legend.title.align = 0.5,
+        legend.key.width = unit(2.5, "cm"),  
+        legend.key.height = unit(0.5, "cm")  
+  ) +
+  guides(
+    linetype = guide_legend(order = 1, 
+                            override.aes = list(size = 1, 
+                                                linewidth = c(0.8, 0.8, 0.4, 0.8, 0.8, 0.8, 0.8))),  # Keep different line sizes
+    color = guide_legend(order = 2, override.aes = list(size = 1)), 
+    size = "none" 
+  )
 
 print(p)
 ggsave("distance_tables/cov_dist_error.png", p, width = 12, height = 8)
